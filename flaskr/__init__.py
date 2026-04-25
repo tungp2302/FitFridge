c6c00bdee87637d50760781d2fb538b6ef3fb98b
ch3m1k4r <103097970+ch3m1k4@users.noreply.github.com>
2026-04-26 00:58:51 +0200

    added fridge page und top nav added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,4 +35,7 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
+    from . import fridge
+    app.register_blueprint(fridge.bp)
+
     return app
